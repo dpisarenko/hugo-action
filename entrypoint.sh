7fbc1c60f3f27a029915818264149507532efee8
Dmitrii Pisarenko <dp@dpisarenko.com>
2020-03-15 19:14:11 +0100

    Debugging the working directory issue.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,24 @@ echo "Starting the Hugo Action"
 
 echo "Trying to find the right directory"
 
+echo "1"
+ls -al /home/runner/work/_temp/_github_home
+
+echo "2"
+ls -al /github/home
+
+echo "3"
+ls -al /home/runner/work/_temp/_github_workflow
+
+echo "4"
+ls -al /github/workflow
+
+echo "5"
 ls -al /home/runner/work/hugo-wp-site/hugo-wp-site
+
+echo "6"
+ls -al /github/workspace
+
 hugo "$@"
 
 echo "#################################################"
